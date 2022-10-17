d4758629167336b932aa94c03eb53d8a92fcf883
Harald Heide Gundersen <harald.heide.gundersen@hotmail.com>
2022-10-17 17:43:16 +0200

    Caustcs
    Finished Decal caustics shader

---
+++ Assets/00hhe00/Help.cs
@@ -0,0 +1,22 @@
+/*
+ * 
+ * Ben Cloward
+ * Water
+ * https://www.youtube.com/watch?v=ipKQt0BxQSA&list=PL78XDi0TS4lGORvoEKCyw_6dO9tzlu6Ox
+ *
+ *
+ *
+ *Caustics
+ *https://www.youtube.com/watch?v=9z6EMsoqLDY&t=0s
+ *
+ *
+ *
+ *Tutorial Brackeys
+ *
+ *
+ * https://www.youtube.com/watch?v=8dejKSbADqE
+ *
+ * 
+ * 
+ * 
+ */ 
